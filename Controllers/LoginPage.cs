cc8cc66d0875ccc34d1167cce61f1db69d4cfcb0
Yuvin Padayachee <ST10083835@vcconnect.edu.za>
2022-11-28 20:14:49 +0200

    BEFORE LOGIN HTML CODE STARTS

--- Controllers/LoginPage.cs
+++ Controllers/LoginPage.cs
@@ -8,41 +8,15 @@ namespace PROG_POE_Part3.Controllers
     
     public class LoginPage : Controller
     {
-        SqlConnection con = new SqlConnection();
-        SqlCommand cmd = new SqlCommand();  
-        SqlDataReader dr;
-
-        [HttpGet]
-        public ActionResult Login()
+        public ActionResult Index()
         {
             return View();
         }
-        void conString()
-        {
-            con.ConnectionString = "Server=tcp:cldv10083835.database.windows.net,1433;Initial Catalog=PROGDB;Persist Security Info=False;User ID=ST10083835;Password=Keenless19;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        }
+
         [HttpPost]
-        public ActionResult Verify(User newUser)
+        public ActionResult Authorize()
         {
-            conString();
-            con.Open();
-            //con.Connection = con;
-            cmd.CommandText = "SELECT Username,Password FROM Users WHERE Username ='"+newUser.Username
-                            +"' and Password = '"+newUser.Password+"'";
-            dr = cmd.ExecuteReader();
-            if (dr.Read())
-            {
-                con.Close();
-                return View();
-            }
-            else
-            {
-                con.Close();
-                return View();
-            }
-
-
-
+            return View();
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PROG_POE_Part3.Models;
 
 public partial class User
 {
+    [DisplayName("Username")]
     public string Username { get; set; }
 
+    [DataType(DataType.Password)]
     public string Password { get; set; }
-
+    
     public string HashedPassword { get; set; }
 
     public virtual ICollection<Module> Modules { get; } = new List<Module>();
