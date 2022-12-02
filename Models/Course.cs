a54c34c76533b0c073e43559af17d2a0757e1669
Yuvin Padayachee <ST10083835@vcconnect.edu.za>
2022-12-02 18:36:09 +0200

    Fixed small bugs

---
+++ Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace PROG_POE_Part3.Models
+{
+    public class Course
+    {
+        public string CourseID { get; set; }
+        public string CourseName { get; set; }  
+        public string Module_Code { get; set; }
+        public string StudyHoursID { get; set; }    
+        public string Username { get; set; }
+        public virtual User UsernameNavigation { get; set; }
+    }
+}
---
+++ Models/StudyHours.cs
@@ -0,0 +1,13 @@
+﻿namespace PROG_POE_Part3.Models
+{
+    public class StudyHours
+    {
+        public string StudyHoursID { get; set; }
+        
+        public string TotalStudyHours { get; set; } 
+
+        public string Module_Code { get; set; }
+        public string Username { get; set; }
+        public virtual User UsernameNavigation { get; set; }
+    }
+}
